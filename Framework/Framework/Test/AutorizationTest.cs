f38d89d57c21973db0033d4f2450e8944877b374
VladislavBr <77299225+VladislavBr@users.noreply.github.com>
2021-02-24 19:33:44 +0200

    Кем бы ты ни был, кем бы ты не стал, помни, где ты был и кем ты стал

---
+++ Framework/Framework/Test/AutorizationTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Pages.Login;
+
+namespace Framework.Test
+{
+	[TestFixture]
+	class AutorizationTest
+	{
+		public IWebDriver driver;
+		[OneTimeSetUp]
+		public void StartTest()
+		{
+			driver = new ChromeDriver();
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+		}
+		[OneTimeTearDown]
+		public void EndTest()
+		{
+			driver.Quit();
+		}
+		[Test]
+		public void AutorizationPageTest()
+		{
+			Account account = new Account("kover18595@geeky83.com", "hahaitwork");
+			//Step
+			Pages.Login.AutorizationPage.AuthorizationPageTest(driver, account);
+
+			Assert.AreEqual("2","2");
+			driver.Quit();
+		}
+	}
+}
