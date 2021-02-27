6b78d7bff2a36fe5d81868ad0c527f0afa29265d
VladislavBr <77299225+VladislavBr@users.noreply.github.com>
2021-02-27 19:27:52 +0200

    browser settings

---
+++ Framework/Framework/Test/BrowserOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Framework.Test
+{
+	class BrowserOption
+	{
+		public static ChromeOptions FirstOption()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArgument("start-maximized");
+			options.AddArgument("disable-infobars");
+			//options.AddArgument("user-data-dir=C:\\Users\\Vlad\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Default");
+			return options;
+		}
+	}
+}
--- Framework/Framework/Test/TestUI/CheckPageItemsTest.cs
+++ Framework/Framework/Test/TestUI/CheckPageItemsTest.cs
@@ -20,8 +20,9 @@ namespace Framework.Test
 		[OneTimeSetUp]
 		public void StartTest()
 		{
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(BrowserOption.FirstOption());
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+			BasePage.goToSite(driver);
 		}
 		[OneTimeTearDown]
 		public void EndTest()
@@ -29,31 +30,25 @@ namespace Framework.Test
 			driver.Quit();
 		}
 		[Test]
-		public void AutorizationPageTest()
+		public void CheckDesktops()
 		{
-			//give
-
 			//Step
-			BasePage.goToSite(driver);
-			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-			//driver.FindElement(By.PartialLinkText("Show All Desktops")).Click();
-			//Actions actions = new Actions(driver);
-			//IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/nav/div[2]/ul/li[1]/a"));
-			//actions.MoveToElement(element).Build().Perform();
+			//driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);			
 			BasePage.MouseHover(driver, BasePage.Desctops);
 			BasePage.ClickButton(driver, "xpatch", BasePage.DesctopsShowAllDesktops);
 			//Verefication
 			Assert.AreEqual("Desktops", ShowAllCategotiesPage.TextCheckRead(driver, ShowAllCategotiesPage.DesctopsCheckXpatch));
-			//Assert.IsTrue(Convert.ToBoolean(ShowAllCategotiesPage.TextCheckRead(driver, ShowAllCategotiesPage.DesctopsCheckXpatch)));
-			//Assert.IsTrue(driver.FindElement(By.XPath("/html/body/div[2]/div/div/h2")));
-			//Assert.AreEqual(driver.PageSource.Contains("Desktope"), true);
+			
+
+		}
+		[Test]
+		public void CheckNotebook()
+		{
 			//step
 			BasePage.MouseHover(driver, BasePage.LaptopsAndNotebooks);
 			BasePage.ClickButton(driver, "xpatch", BasePage.LaptopsAndNotebooksShowAll);
-			Thread.Sleep(2000);
 			//Verefication
 			Assert.AreEqual("Laptops & Notebooks", ShowAllCategotiesPage.TextCheckRead(driver, ShowAllCategotiesPage.NotebookAndLapptopeTextCheck));
-
 		}
 	}
 }
