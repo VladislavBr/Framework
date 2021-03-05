09b5e1660624f2a70ebf79c7bf779602a3ad7433
VladislavBr <77299225+VladislavBr@users.noreply.github.com>
2021-03-05 15:58:12 +0200

    арі

--- Framework V2/Framework/Test/TestUI/AutorizationTest.cs
+++ Framework V2/Framework/Test/TestUI/AutorizationTest.cs
@@ -25,26 +25,10 @@ namespace Framework.Test
 		[OneTimeSetUp]
 		public void StartTest()
 		{
-			//_driver = new ChromeDriver(BrowserOption.BrowserSettings());
-			//_driver = new ChromeDriver(BrowserOption.BrowserSettings());
-			//driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-			//_driver = new ChromeDriver();
-			//BrowserOption.OptionForSelenoid();
+			
 			BrowserOption br = new BrowserOption();
 			_driver = br.OptionForSelenoid();
-			//var driverOptions = new ChromeOptions();
-			//var runName = GetType().Assembly.GetName().Name;
-			//var timestamp = $"{DateTime.Now:yyyyMMdd.HHmm}";
-			//driverOptions.AddArgument("start-maximized");
-			//driverOptions.AddAdditionalCapability("name", runName, true);
-			//driverOptions.AddAdditionalCapability("videoName", $"{runName}.{timestamp}.mp4", true);
-			//driverOptions.AddAdditionalCapability("logName", $"{runName}.{timestamp}.log", true);
-			//driverOptions.AddAdditionalCapability("enableVNC", true, true);
-			//driverOptions.AddAdditionalCapability("enableVideo", true, true);
-			//driverOptions.AddAdditionalCapability("enableLog", true, true);
-			//driverOptions.AddAdditionalCapability("screenResolution", "1920x1080x24", true);
-			//_driver = new RemoteWebDriver(new Uri("http://127.0.0.1:8080/wd/hub"), driverOptions);
-			//_driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+			
 		}
 		[OneTimeTearDown]
 		public void EndTest()
